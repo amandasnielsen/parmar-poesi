95bc94fc125bf1f394a14bcb02a2b4fa3fc6a72f
Amanda Nielsen <amandasn202@gmail.com>
2025-04-07 21:34:51 +0200

    added button for portfolio

---
+++ back-button.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const button = document.getElementById("myButton");
+  
+	button.addEventListener("click", () => {
+		window.location.href = "../index.html";
+	});
+  });
